b566ba9643ec42d479c86396ff84483aa75bf78f
Jon-Hart <jhart@scripps.edu>
2014-11-03 21:03:08 -0800

    Need to figure out the x axis for the time series

---
+++ plot2.R
@@ -0,0 +1,25 @@
+# Download data
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+              "power.zip",
+              method="curl")
+unzip("power.zip")
+
+# Load the data in
+power.file <- file("household_power_consumption.txt")
+power.lines <- readLines(power.file)
+keep <- grepl("^[12]/2/2007",  power.lines)
+power.filter <- power.lines[keep]
+power.data <- read.table(text=power.filter, sep=";", header=F, na.strings="?",
+                         colClasses = c("character", "character", rep("numeric", 7)))
+colnames(power.data) <- unlist(strsplit(power.lines[1], ";"))
+power.data$datetime <- strptime(paste(power.data$Date, power.data$Time), 
+                                format="%d/%m/%Y %H:%M:%S")
+
+# Construct plot 1
+library(xts)
+power.ts <- xts(power.data$Global_active_power, power.data$datetime)
+
+png("plot1.png", 480, 480)
+
+
+dev.off()
