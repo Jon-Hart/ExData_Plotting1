b1ccce3086d42fc7a6cb8afc331af91b521fbd16
Jon-Hart <jhart@scripps.edu>
2014-11-03 21:29:49 -0800

    Plot 3 solution

--- plot3.R
+++ plot3.R
@@ -15,11 +15,15 @@ colnames(power.data) <- unlist(strsplit(power.lines[1], ";"))
 power.data$datetime <- strptime(paste(power.data$Date, power.data$Time), 
                                 format="%d/%m/%Y %H:%M:%S")
 
-# Construct plot 2
+# Construct plot 3
 
-png("plot2.png", 480, 480)
-plot(power.data$Global_active_power, type="l",
-     xlab="", ylab="Global Active Power (kilowatts)", xaxt="n"
+png("plot3.png", 480, 480)
+plot(power.data$Sub_metering_1, type="l",
+     xlab="", ylab="Energy sub metering", xaxt="n"
 )
+lines(power.data$Sub_metering_2, col="red")
+lines(power.data$Sub_metering_3, col="blue")
 axis(1, c(0, 1440, 2880), labels=c("Thu", "Fri", "Sat"))
+legend("topright", lty=1, col=c("black", "red", "blue"),
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.off()
